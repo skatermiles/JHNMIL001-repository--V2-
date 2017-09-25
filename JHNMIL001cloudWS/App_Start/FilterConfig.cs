03c3bd37c79db368bc8eca7ed46a9ed2edf0a3e0
skatermiles <infomilesjohnson@gmail.com>
2017-09-25 17:48:35 +0200

    change
    change

---
+++ JHNMIL001cloudWS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JHNMIL001cloudWS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ JHNMIL001cloudWS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JHNMIL001cloudWS.MvcApplication" Language="C#" %>
